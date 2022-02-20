1551501a7b6a2d62676899b68a8b64edea9482f8
jasmineblinks <puenehfaith@gmail.com>
2022-02-20 02:33:31 +0100

    added the javascript

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-// function handleSubmit() {
-//   let quotes = {
-//     "— Henry Ford":
-//       '"Whether you think you can or you think you can’t, you’re right."',
-//     " — Alice Walker":
-//       '"The most common way people give up their power is by thinking they don’t have any."',
-//     "— Herb Brooks": '"Risk something or forever sit with your dreams."',
-//     "— Steve Jobs":
-//       '"The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle.”"',
-//     "— Theodore Roosevelt": '"Believe you can and you’re halfway there."',
-//     " — Vince Lombardi":
-//       '"Perfection is not attainable, but if we chase perfection we can catch excellence."',
-//     " — Audrey Hepburn":
-//       'Nothing is impossible, the word itself says ‘I’m possible’!."',
-//   };
-//   let randomAuthors = Object.keys(quotes);
-//   let authorPicker =
-//     randomAuthors[Math.floor(Math.random() * randomAuthors.length)];
-//   let quote = quotes[authorPicker];
-//   document.getElementById("quote").innerHTML = quote;
-//   document.getElementById("author").innerHTML = authorPicker;
-// }
-// handleSubmit();
+function handleSubmit() {
+  let quotes = {
+    "— Henry Ford":
+      '"Whether you think you can or you think you can’t, you’re right."',
+    " — Alice Walker":
+      '"The most common way people give up their power is by thinking they don’t have any."',
+    "— Herb Brooks": '"Risk something or forever sit with your dreams."',
+    "— Steve Jobs":
+      '"The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. Don’t settle.”"',
+    "— Theodore Roosevelt": '"Believe you can and you’re halfway there."',
+    " — Vince Lombardi":
+      '"Perfection is not attainable, but if we chase perfection we can catch excellence."',
+    " — Audrey Hepburn":
+      'Nothing is impossible, the word itself says ‘I’m possible’!."',
+  };
+  //   let randomAuthors = Object.keys(quotes);
+  //   let authorPicker =
+  //     randomAuthors[Math.floor(Math.random() * randomAuthors.length)];
+  //   let quote = quotes[authorPicker];
+  //   document.getElementById("quote").innerHTML = quote;
+  //   document.getElementById("author").innerHTML = authorPicker;
+}
+handleSubmit();
